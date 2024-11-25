1d1bd5b4fee735ffa6ec157c906c54b3e27e044a
AlitPurnama <alitpurnama13@gmail.com>
2024-11-25 14:48:22 +0900

    initial commit (boring)

---
+++ lua/plugins/files.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "stevearc/oil.nvim",
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {
+      default_file_explorer = true,
+      skip_confirm_for_simple_edits = true,
+    },
+    keys = {
+      { "-", "<cmd>Oil<cr>", mode = { "n" } },
+    },
+    dependencies = { { "echasnovski/mini.icons", opts = {} } },
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    enabled = false,
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+  },
+}
